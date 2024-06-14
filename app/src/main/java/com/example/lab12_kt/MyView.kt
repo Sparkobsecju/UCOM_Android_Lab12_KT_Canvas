c1e95987a950dd92c9b7d96c732ecaecaef55ffb
Sivan Chen <ryan1825114@gmail.com>
2024-06-14 14:22:43 +0800

    draw two circle with Canvas and Paint

--- app/src/main/java/com/example/lab12_kt/MyView.kt
+++ app/src/main/java/com/example/lab12_kt/MyView.kt
@@ -15,6 +15,18 @@ class MyView(context: Context, attributeView: AttributeSet?, defaultStyle: Int) 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         paintBackground(canvas)
+        paintCircle(canvas)
+    }
+
+    private fun paintCircle(canvas: Canvas) {
+        val paint = Paint()
+        paint.style = Paint.Style.FILL
+        paint.color = Color.RED
+        paint.isAntiAlias = true // 抗鋸齒
+        canvas.drawCircle(300f, 300f, 200f, paint)
+        paint.color = Color.BLUE
+        paint.isAntiAlias = false
+        canvas.drawCircle(400f, 400f, 200f, paint)
     }
 
     private fun paintBackground(canvas: Canvas) {
